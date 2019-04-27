5900839662c3701fa1873d4153f627566b48dccc
KeshDroid <ktrajapaksha27@gmail.com>
2019-04-27 17:35:21 +0530

    signIn signup

--- app/src/main/java/lk/ac/kln/pizzaloop/SignIn.java
+++ app/src/main/java/lk/ac/kln/pizzaloop/SignIn.java
@@ -1,13 +1,30 @@
 package lk.ac.kln.pizzaloop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 openSignUp();
+            }
+        });
     }
+        public void openSignUp(){
+            Intent intent = new Intent(SignIn.this,SignUp.class);
+            startActivity(intent);
+        }
 }
